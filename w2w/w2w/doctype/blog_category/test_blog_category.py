9d2d03277857f685c2be7a876cf4ef7af3301992
naveen <naveen@naveen.myguest.virtualbox.org>
2023-09-23 00:37:59 -0700

    first commit

---
+++ w2w/w2w/doctype/blog_category/test_blog_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Neveka Berl J and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBlogCategory(FrappeTestCase):
+	pass
---
+++ w2w/w2w/doctype/blogger/blogger.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Neveka Berl J and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Blogger', {
+	// refresh: function(frm) {
+
+	// }
+});
